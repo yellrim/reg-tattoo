a5274a0829175e6dc45b3b85e25d1ff4f9e0ac5f
yellrim <93649193+yellrim@users.noreply.github.com>
2022-05-02 16:33:59 +0900

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,14 @@
+function btnMb(){
+    document.getElementById('btn-mb').style.display = 'flex';
+    document.getElementById('btn-close').style.display = 'block';
+}
+
+function btnClose(){
+    document.getElementById('btn-mb').style.display = 'none';
+    document.getElementById('btn-close').style.display = 'none';
+}
+
+function navClose(){
+    document.getElementById('btn-mb').style.display = 'none';
+    document.getElementById('btn-close').style.display = 'none';
+}
